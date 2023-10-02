a1c5529c11b73a0738d50bf2227a0b4103b4a0dc
QuixDK <stroganovdanila@outlook.com>
2023-10-02 19:28:24 +0300

    now you can make screenshot of any selected area ^_^

--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -49,9 +49,17 @@ public class MainForm implements Runnable {
         translateAreaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+            JFrame areaForTranslation = new JFrame();
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            areaForTranslation.setSize(screenSize.width, screenSize.height);
+            areaForTranslation.setLocation(screenSize.width/2-areaForTranslation.getSize().width/2, screenSize.height/2-areaForTranslation.getSize().height/2);
+            areaForTranslation.setUndecorated(true);
+            areaForTranslation.setOpacity(0.5f);
+            areaForTranslation.setVisible(true);
 
 
-                mainFrame.addMouseListener(new MouseListener() {
+
+                areaForTranslation.addMouseListener(new MouseListener() {
                     double x = 0;
                     double y = 0;
 
@@ -70,6 +78,7 @@ public class MainForm implements Runnable {
 
                     @Override
                     public void mouseReleased(MouseEvent e) {
+                        areaForTranslation.setVisible(false);
                         double x2 = MouseInfo.getPointerInfo().getLocation().getX();
                         double y2 = MouseInfo.getPointerInfo().getLocation().getY();
                         double width;
